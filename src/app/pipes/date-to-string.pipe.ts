744ae379b3c53898768cf272fb566219f9a7b168
Matias Frith <frithmatias@gmail.com>
2021-03-21 11:50:20 -0300

    2.0.7 fix styles +date-to-string pipe time formats

--- src/app/pipes/date-to-string.pipe.ts
+++ src/app/pipes/date-to-string.pipe.ts
@@ -6,18 +6,35 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DateToStringPipe implements PipeTransform {
   transform(time: Date, format: string): string {
 
-    var options = {
-      weekday: 'long',
-      // year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit',
-      // second: '2-digit',
-      hour12: false
-    };
+    var options = {};
 
-    var str: string = new Date(time).toLocaleDateString('es-AR', options);
-    return str;
+    switch (format) {
+      case 'full': // Sábado, 20 de Marzo de 2021 21:05
+        options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
+        break;
+      case 'date-full': // Sábado, 20 de Marzo de 2021
+        options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        break;
+      case 'date-long': // Sábado, 20 de Marzo
+        options = { weekday: 'long', month: 'long', day: 'numeric' };
+        break;
+      case 'date': // 20 de Marzo
+        options = { month: 'long', day: 'numeric' };
+        break;
+      case 'date-short': // 20-03
+        options = { month: 'numeric', day: 'numeric' };
+        break;
+      case 'time-full': // 21:05:34
+        options = { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
+        break;
+      case 'time-12': // 9:05 PM
+        options = { hour: '2-digit', minute: '2-digit', hour12: true };
+        break;
+      case 'time-24': // 21:05
+        options = { hour: '2-digit', minute: '2-digit', hour12: false };
+        break;
+    }
+
+    return new Date(time).toLocaleString('es-AR', options);;
   }
 }
